7f2f1ecc7239742826e3c1d1aad3ce4828075e15
Miguel Marques <miguelcgmarques@gmail.com>
2022-04-17 14:21:02 +0100

    Serve static pages from twitch-platform-filter

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Println(w, "bye bye")
-}
-
 func main() {
-	http.HandleFunc("/hello", hello)
-
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", http.FileServer(http.Dir("./twitch-platform-filter/html")))
 }
