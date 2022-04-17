0a5ea208785ce5378cf4708b74beec54f6ce959b
Miguel Marques <miguelcgmarques@gmail.com>
2022-04-17 18:45:15 +0100

    Serve and update twitch-platform-filter

--- main.go
+++ main.go
@@ -1,9 +1,59 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-co-op/gocron"
 )
 
+func getActiveConsoles() []string {
+	files, err := ioutil.ReadDir("./twitch-platform-filter/html")
+	if err != nil {
+		return nil
+	}
+
+	var activeConsoles []string
+	for _, file := range files {
+		filename := file.Name()
+		if strings.Contains(filename, ".html") {
+			filenameSplit := strings.Split(filename, ".")
+			_, err := strconv.Atoi(filenameSplit[0])
+			if err == nil {
+				activeConsoles = append(activeConsoles, filenameSplit[0])
+			}
+		}
+	}
+	return activeConsoles
+}
+
+func refreshLiveGames() {
+	fmt.Println("Refreshing live games.")
+	activeConsoles := getActiveConsoles()
+	if activeConsoles == nil {
+		fmt.Println("No active consoles found!")
+		return
+	}
+
+	commands := append([]string{"twitch-platform-filter/twitch-filter.py", "silent"}, activeConsoles...)
+
+	cmd := exec.Command("python", commands...)
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(string(stdout))
+}
+
 func main() {
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(1).Hours().Do(refreshLiveGames)
+	s.StartAsync()
 	http.ListenAndServe(":3000", http.FileServer(http.Dir("./twitch-platform-filter/html")))
 }
